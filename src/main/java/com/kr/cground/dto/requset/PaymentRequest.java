248cb9b7cc6d232353b511b4c9d3ea8a822aca22
beomshin.kim <chzh859@naver.com>
2025-06-08 22:49:45 +0900

    feat: timezone변경

--- src/main/java/com/kr/cground/dto/requset/PaymentRequest.java
+++ src/main/java/com/kr/cground/dto/requset/PaymentRequest.java
@@ -1,5 +1,7 @@
 package com.kr.cground.dto.requset;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter
@@ -9,10 +11,13 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class PaymentRequest {
 
+    @NotBlank(message = "주문번호가 누락되었습니다.")
     private String orderNumber;
 
+    @NotNull(message = "금액이 누락되었습니다.")
     private Integer amount;
 
+    @NotBlank(message = "유저번호가 누락되었습니다.")
     private String userId;
 
 }
