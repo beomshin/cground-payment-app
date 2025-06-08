55d1b2d3426a2bb1bbe60cf728aa0278508ac606
beomshin.kim <chzh859@naver.com>
2025-06-08 21:27:44 +0900

    feat: timezone변경

--- src/main/java/com/kr/cground/CgroundPaymentAppApplication.java
+++ src/main/java/com/kr/cground/CgroundPaymentAppApplication.java
@@ -1,12 +1,20 @@
 package com.kr.cground;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @ServletComponentScan
 public class CgroundPaymentAppApplication {
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CgroundPaymentAppApplication.class, args);
